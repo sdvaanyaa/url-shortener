63827f6249f962cde011ffa5e62e86eb7412af34
Ivan A <sd.vaanyaa@yandex.ru>
2025-03-13 13:47:58 +0300

    feat: add basic auth to url save and delete endpoints

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -51,9 +51,18 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	router.Post("/url", save.New(log, storage))
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+		r.Post("/", save.New(log, storage))
+	})
+
+	router.With(middleware.BasicAuth("url-shortener", map[string]string{
+		cfg.HTTPServer.User: cfg.HTTPServer.Password,
+	})).Delete("/{alias}", delete.New(log, storage))
+
 	router.Get("/{alias}", redirect.New(log, storage))
-	router.Delete("/{alias}", delete.New(log, storage))
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
